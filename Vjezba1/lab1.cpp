5e17e88bdf6df3755ce639a7181bf1cd9d683a6b
AntonijaKezic <87046226+AntonijaKezic@users.noreply.github.com>
2024-10-14 09:57:43 +0200

    Update lab1.cpp

--- Vjezba1/lab1.cpp
+++ Vjezba1/lab1.cpp
@@ -1,41 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
-//#define ERROR_OF 1
-#define MAX 1024
+#define MAX 1024 
 #include <stdio.h>
 
-int NumberOfRows(const char*);
+int NumberOfRows(const char*); 
 
 int main() {
-	int numOfRows = 0;
-	numOfRows = NumberOfRows("imedat.txt");
-
-	printf("Hello World");
-	return 0;
+    int numOfRows = 0;
+    // Poziv funkcije koja broji redove u datoteci "imedat.txt"
+    numOfRows = NumberOfRows("imedat.txt");
+    return 0;
 }
 
 int NumberOfRows(const char* fileName) {
-	FILE* fp = NULL;
-	int numOfRows = 0;
-	char buffer[MAX] = {};
+    FILE* fp = NULL;
+    int numOfRows = 0;
+    char buffer[MAX] = {}; // Prazan buffer za čitanje redaka
 
-	fp = fopen(fileName, "r");
-	if (!fp) {
-		return -1;
+    fp = fopen(fileName, "r");
+    if (!fp) {
+        // Ako se datoteka ne može otvoriti, vraća -1 kao oznaku greške
+        return -1;
+    }
+    while (!feof(fp)) {
+        fgets(buffer, MAX, fp);
+        numOfRows++;
+    }
+    fclose(fp);
+
+    return numOfRows; // Vraća broj redova u datoteci
 }
-	while (!feof(fp)) {
-		fgets(buffer, );
-		numOfRows++;
-	}
-
-
-
-/* 
-int OpenFile(char *fileName) {
-	FILE *fp = NULL;
-	fp = fopen(fileName, "r");
-	if (fp == NULL) {
-		printf("");
-		return ERROR;
-	}
-}
-*/
